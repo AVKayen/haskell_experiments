1e1a85245c87792a50372cc6900a482ff945acba
AVKayen <aganitnt@gmail.com>
2024-10-10 20:16:21 +0200

    update mrufka.hs

--- mrufka/mrufka.hs
+++ mrufka/mrufka.hs
@@ -1,7 +1,7 @@
 -- Solves https://arkusze.pl/maturalne/informatyka-2014-grudzien-probna-rozszerzona-2.pdf Zadanie 4.1 - 4.3 (Without graphs)
 -- Outputs to answers.txt
-
 module Main where
+import Text.Printf (printf)
 
 formatData :: [(Double, Double, Double)] -> String
 formatData dataPoints = unlines $ map (\(t, x, y) -> unwords [show t, show x, show y]) dataPoints
@@ -21,12 +21,14 @@ dist (x1, y1) (x2, y2) = sqrt ((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
 
 -- 4.1 | 4.2
 trace_point :: (Double, Double, Double, Double, Double) -> (Double -> Double -> Double -> Bool) -> [(Double, Double, Double)]
-trace_point (radius, period, dt, time, speed) condition
-    | condition time x y = []
-    | otherwise = (time, x, y) : trace_point (radius + (speed * dt), period, dt, time + dt, speed) condition
-    where
-        x = fx (radius, time, period)
-        y = fy (radius, time, period)
+trace_point (radius, period, dt, time, speed) condition =
+    if condition time x y
+    then [(time, x, y)]  -- Return the last point that didn't satisfy the condition
+    else (time, x, y) : trace_point (radius + (speed * dt), period, dt, time + dt, speed) condition
+  where
+    x = fx (radius, time, period)
+    y = fy (radius, time, period)
+
 
 -- 4.3
 accum_dist :: [(Double, Double, Double)] -> Double -> Double -> Double
@@ -41,14 +43,16 @@ map_dist ((t, nx, ny) : ndata) x y = dist (x, y) (nx, ny) : (map_dist ndata nx n
 main :: IO ()
 main = do
     let results41 = trace_point (5.0, 12.5, 0.05, 3, 0) (\_ x y -> y > x)
-        results42 = trace_point (0, 10.0, 0.5, 0, 1) (\time _ _ -> time > 10)
+        results42 = trace_point (0, 10.0, 0.5, 0, 1) (\time _ _ -> time >= 10)
         lastT = last (map (\(t, _, _) -> t) results41)
         distances = map_dist results42 0 0
         totalAccumDist = accum_dist results42 0 0
 
     writeFile "answers.txt" $ 
-        "4.1\ntime x y\n" ++ formatData results41 ++
-        "\n\nTime: " ++ show lastT ++
-        "\n\n4.2\ntime x y\n" ++ formatData results42 ++
-        "\n\n4.3\ndistances\n" ++ formatDataDist distances ++
-        "\n\nTotal Distance: " ++ show totalAccumDist
+
+        printf "4.1\ntime x y\n%s\n\nTime: %.2f\n\n4.2\ntime x y\n%s\n\n4.3\ndistances\n%s\n\nTotal Distance: %.4f" 
+            (formatData results41) 
+            lastT 
+            (formatData results42) 
+            (formatDataDist distances)
+            totalAccumDist
